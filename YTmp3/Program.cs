ea5960c2b5dabdc1cb552a04ad84ab6f8488fd0e
S Smith <stevensmith.ome@gmail.com>
2019-11-05 13:04:20 +0800

    Added automatic opening of download URLs for dependencies

--- YTmp3/Program.cs
+++ YTmp3/Program.cs
@@ -1,59 +1,55 @@
 ﻿using System;
 using System.Windows.Forms;
 using static YTmp3.Downloader;
+using System.IO;
 
 namespace YTmp3
 {
     public static class Program
     {
+
+        public static bool ExistsOnPath(string fileName)
+        {
+            return GetFullPath(fileName) != null;
+        }
+
+        public static string GetFullPath(string fileName)
+        {
+            if (File.Exists(fileName))
+               return Path.GetFullPath(fileName);
+
+            var values = Environment.GetEnvironmentVariable("PATH");
+            foreach (var path in values.Split(Path.PathSeparator))
+            {
+                var fullPath = Path.Combine(path, fileName);
+                if (File.Exists(fullPath))
+                    return fullPath;
+            }
+            return null;
+        }
+        public static void alert(string text) {
+            MessageBox.Show(text, "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         public static void Main(string[] args)
         {
-            if (args.Length > 0)
-            {
-                DLOptions choice = DLOptions.MP3;
-                string qualityoptions = string.Join(",", Enum.GetNames(typeof(DLOptions))).ToLower();
-                string path = Environment.ExpandEnvironmentVariables(@"%USERPROFILE%\Music");
-                foreach (string arg in args)
-                {
-                    if (arg[0] == '/') {
-                        if (arg[1] == 'h' || arg[1] == '?')
-                        {
-                            Console.WriteLine("YTmp3");
-                            Console.WriteLine("Usage: YTmp3 <urls...> [/h] [/q{" + qualityoptions + "}]");
-                            return;
-                        }
-                        if (arg[1] == 'q')
-                        {
-                            if (!Enum.TryParse(arg.Substring(2), out choice))
-                            {
-                                Console.WriteLine("Invalid quality option! Choices: " + qualityoptions);
-                            }
-                        }
-                        if (arg[1] == 'p')
-                        {
-                            path = arg.Substring(2);
-                        }
-                        continue;
-                    }
-                    Console.WriteLine("Starting on " + arg);
-                    Downloader.Download(arg, choice, path);
-                }
+            if (!ExistsOnPath("ffmpeg.exe") || !ExistsOnPath("ffprobe.exe")) {
+                alert("FFMPEG missing, opening browser. Please ensure ffmpeg.exe and ffprobe.exe are in the same location as YTmp3.exe and restart the program.");
+                System.Diagnostics.Process.Start("http://ffmpeg.zeranoe.com/builds/");
                 return;
             }
-
-            if (Type.GetType("Mono.Runtime") != null)
-            {
-                Console.WriteLine("This probably won't work! Try YTmp3 /h");
-            } else
-            {
-                Application.EnableVisualStyles();
-                Application.SetCompatibleTextRenderingDefault(false);
-                Application.Run(new Downloader());
+            if (!ExistsOnPath("youtube-dl.exe")) {
+                alert("youtube-dl missing, opening browser. Please ensure youtube-dl.exe is in the same location as YTmp3.exe and restart the program.");
+                System.Diagnostics.Process.Start("https://yt-dl.org/downloads/latest/youtube-dl.exe");
+                return;
             }
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Downloader());
         }
 
 
